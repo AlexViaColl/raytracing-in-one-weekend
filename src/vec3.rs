01ca82d1a62bb44860bc76ac2872fdd6a5a2ac3b
AlexViaColl <alexviacoll@gmail.com>
2022-05-23 23:28:00 +0200

    Implement Vec3 and utilities

---
+++ src/vec3.rs
@@ -0,0 +1,65 @@
+use std::ops::{Add, AddAssign, DivAssign, Mul, MulAssign, Neg, Sub};
+
+#[derive(Default, Debug)]
+pub struct Vec3 {
+    pub e: [f64; 3],
+}
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+impl Vec3 {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
+        Self { e: [e0, e1, e2] }
+    }
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self::new(-self.e[0], -self.e[1], -self.e[2])
+    }
+}
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, t: f64) -> Self {
+        Self::new(self.e[0] * t, self.e[1] * t, self.e[2] * t)
+    }
+}
+
+pub fn dot(u: &Vec3, v: &Vec3) -> f64 {
+    u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
+}
+pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3::new(
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0],
+    )
+}
+pub fn unit_vector(v: Vec3) -> Vec3 {
+    let f = 1.0 / v.length();
+    v * f
+}
+
+pub fn write_color(pixel_color: Color) {
+    println!(
+        "{} {} {}",
+        (255.999 * pixel_color.x()) as i32,
+        (255.999 * pixel_color.y()) as i32,
+        (255.999 * pixel_color.z()) as i32
+    );
+}
