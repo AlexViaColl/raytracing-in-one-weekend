76ad35f5b0fd007da2cb7714bdc578a99a445291
AlexViaColl <alexviacoll@gmail.com>
2022-05-24 22:58:11 +0200

    Final scene

--- src/main.rs
+++ src/main.rs
@@ -20,31 +20,32 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 500;
     let max_depth = 50;
 
     // World
-    let mut world = HittableList::default();
+    let world = random_scene();
+    //let mut world = HittableList::default();
 
-    let material_group = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    //let material_center = Box::new(Dielectric::new(1.5));
-    //let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
-    let material_left = Box::new(Dielectric::new(1.5));
-    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    //let material_group = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    //let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    ////let material_center = Box::new(Dielectric::new(1.5));
+    ////let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    //let material_left = Box::new(Dielectric::new(1.5));
+    //let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
-    world.add(Box::new(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, material_group)));
-    world.add(Box::new(Sphere::new(Point3::new( 0.0,    0.0, -1.0),   0.5, material_center)));
-    world.add(Box::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0),   0.5, material_left.clone())));
-    world.add(Box::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0), -0.45, material_left)));
-    world.add(Box::new(Sphere::new(Point3::new( 1.0,    0.0, -1.0),   0.5, material_right)));
+    //world.add(Box::new(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, material_group)));
+    //world.add(Box::new(Sphere::new(Point3::new( 0.0,    0.0, -1.0),   0.5, material_center)));
+    //world.add(Box::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0),   0.5, material_left.clone())));
+    //world.add(Box::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0), -0.45, material_left)));
+    //world.add(Box::new(Sphere::new(Point3::new( 1.0,    0.0, -1.0),   0.5, material_right)));
 
     // Camera
-    let from = Point3::new(3.0, 3.0, 2.0);
-    let at = Point3::new(0.0, 0.0, -1.0);
+    let from = Point3::new(13.0, 2.0, 3.0);
+    let at = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (from - at).length();
-    let apperture = 2.0;
+    let dist_to_focus = 10.0;
+    let apperture = 0.1;
     let cam = Camera::new(from, at, vup, 20.0, aspect_ratio, apperture, dist_to_focus);
 
     // Render
@@ -64,6 +65,68 @@ fn main() {
         }
     }
     eprintln!("\nDone.")
+}
+
+fn random_scene() -> HittableList {
+    let mut world = HittableList::default();
+
+    let ground_material = Box::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_double();
+            let center = Point3::new(
+                a as f64 + 0.9 * random_double(),
+                0.2,
+                b as f64 + 0.9 * random_double(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Box<dyn Material> = if choose_mat < 0.8 {
+                    // Diffuse
+                    let albedo = Color::random() * Color::random();
+                    Box::new(Lambertian::new(albedo))
+                } else if choose_mat < 0.95 {
+                    // Metal
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = random_double_range(0.0, 0.5);
+                    Box::new(Metal::new(albedo, fuzz))
+                } else {
+                    // Glass
+                    Box::new(Dielectric::new(1.5))
+                };
+                world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+            }
+        }
+    }
+
+    let material1 = Box::new(Dielectric::new(1.5));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2 = Box::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3 = Box::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    world
 }
 
 #[rustfmt::skip]
