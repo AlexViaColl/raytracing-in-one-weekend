d3180f0d2ff653f1d340bd7d7832004a34511fb1
AlexViaColl <alexviacoll@gmail.com>
2022-05-24 08:19:40 +0200

    A list of hittable objects

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,6 +1,7 @@
 use super::ray::*;
 use super::vec3::*;
 
+#[derive(Default, Copy, Clone)]
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
@@ -20,4 +21,39 @@ impl HitRecord {
 
 pub trait Hittable {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+}
+
+#[derive(Default)]
+pub struct HittableList {
+    pub objects: Vec<Box<dyn Hittable>>,
+}
+impl HittableList {
+    pub fn new(object: Box<dyn Hittable>) -> Self {
+        Self {
+            objects: vec![object],
+        }
+    }
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+    pub fn add(&mut self, object: Box<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::default();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(r, t_min, t_max, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+
+        hit_anything
+    }
 }
